deae63fe82332537c3a948c47d7fc1b6cad8a48a
anupama <anupama_avula@yahoo.com>
2020-06-05 10:35:12 -0400

    Final Commit

---
+++ EtchASketch.java
@@ -0,0 +1,63 @@
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import javax.swing.JFrame;
+
+public class EtchASketch extends Canvas
+{
+	int x, y;
+	Color cur;
+	
+	public static void main( String[] args )
+	{
+		JFrame win = new JFrame("Use the arrow keys!");
+		win.setSize(1024,768);
+		win.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		win.add( new EtchASketch() );
+		win.setVisible(true);
+	}
+
+	public EtchASketch()
+	{
+		enableEvents(java.awt.AWTEvent.KEY_EVENT_MASK);
+		requestFocus();
+		x = 300;
+		y = 300;
+        cur = Color.red;
+        
+	}
+
+	public void paint( Graphics g )
+	{
+		g.setColor(cur);
+		g.fillOval(x, y, 20, 20);
+	}
+
+	public void update( Graphics g )
+	{
+		paint(g);
+	}
+
+	public void processKeyEvent(KeyEvent e)
+	{
+		// this method automatically gets called with they press a key
+		if ( e.getID() == KeyEvent.KEY_PRESSED )
+		{
+			if ( e.getKeyCode() == KeyEvent.VK_UP )
+				y -= 5;
+			if ( e.getKeyCode() == KeyEvent.VK_DOWN )
+                y += 5;
+             if ( e.getKeyCode() == KeyEvent.VK_RIGHT )
+                x += 5;
+             if ( e.getKeyCode() == KeyEvent.VK_LEFT )
+                x -= 5;
+
+			// and we manually call paint() again to redraw
+			repaint();
+		}
+	}
+	
+	public boolean isFocusable()
+	{
+		return true;
+	}
+}
